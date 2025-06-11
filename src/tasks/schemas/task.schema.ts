d696df30e85e65954b434c8842dc14a848b03049
EnzoLavieri <devenzolavieri@gmail.com>
2025-06-11 15:33:06 -0300

    add task schema props

---
+++ src/tasks/schemas/task.schema.ts
@@ -0,0 +1,22 @@
+import {Prop, Schema, SchemaFactory} from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type TaskDocument = Task & Document;
+
+@Schema()
+export class Task {
+  @Prop({ required: true })
+  title: string;
+
+  @Prop()
+  description: string;
+
+  @Prop({ default: false })
+  completed: boolean;
+
+  @Prop({ default: Date.now })
+  createdAt: Date;
+
+  @Prop({ default: Date.now })
+  updatedAt: Date;
+}
