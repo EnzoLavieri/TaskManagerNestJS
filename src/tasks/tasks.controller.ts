3ce1388774fd9580e849ed572ae9c1912949957c
EnzoLavieri <devenzolavieri@gmail.com>
2025-06-11 15:46:31 -0300

    doing task controller and service

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,33 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete, Put } from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { Task } from './schemas/task.schema';
 
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+    constructor(private readonly tasksService: TasksService) {}
+
+    @Post()
+    async create(@Body() task: Partial<Task>): Promise<Task> {
+        return this.tasksService.create(task);
+    }
+
+    @Get()
+    async findAll(): Promise<Task[]> {
+        return this.tasksService.findAll();
+    }
+
+    @Get(':id')
+    async findOne(@Param('id') id: string): Promise<Task> {
+        return this.tasksService.findOne(id);
+    }
+
+    @Put(':id')
+    async update(@Param('id') id: string, @Body() data: Partial<Task>): Promise<Task> {
+        return this.tasksService.update(id, data);
+    }
+
+    @Delete(':id')
+    async delete(@Param('id') id: string): Promise<void> {
+        return this.tasksService.delete(id);
+    }
+}
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,37 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Task, TaskDocument } from './schemas/task.schema';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+    constructor(@InjectModel(Task.name) private taskModel: Model<TaskDocument>) {}
+
+    async create(task: Partial<Task>): Promise<Task> {
+        const createdTask = new this.taskModel(task);
+        return createdTask.save();
+    }
+
+    async findAll(): Promise<Task[]> {
+        return this.taskModel.find().exec();
+    }
+
+    async findOne(id: string): Promise<Task> {
+        const task = await this.taskModel.findById(id).exec();
+        if (!task) {
+            throw new NotFoundException(`Task with ID ${id} not found`);
+        }}
+
+    async update(id: string, data: Partial<Task>): Promise<Task> {
+        const task = await this.taskModel.findByIdAndUpdate(id, data, { new: true });   
+        if (!task) {
+            throw new NotFoundException(`Task with ID ${id} not found`);
+        }}
+
+    async delete(id: string): Promise<void> {
+        const task = await this.taskModel.findByIdAndDelete(id).exec();
+        if (!task) {
+            throw new NotFoundException(`Task with ID ${id} not found`);
+        }
+    }
+}
